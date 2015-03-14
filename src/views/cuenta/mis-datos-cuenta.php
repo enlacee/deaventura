460d4e652baca3cda6156588a102934dff8e832e
Anibal Copitan <acopitan@gmail.com>
2015-03-14 11:41:12 -0500

    add layout perfil aventurero

--- src/views/cuenta/mis-datos-cuenta.php
+++ src/views/cuenta/mis-datos-cuenta.php
@@ -2,7 +2,7 @@
 
 <div class="red">
     <div class="titulo-for">
-        <h2><span class="glyphicon glyphicon-user"></span> MI PERFIL</h2>
+        <h2><span class="glyphicon glyphicon-user ico-account"></span> MI PERFIL</h2>
     </div>
 
 
@@ -12,15 +12,11 @@
             <li><a href="#tabs-2">Deportes</a></li>
             <li><a href="#tabs-3">Foto</a></li>
         </ul>
+        
+        <!-- TAB General-->
         <div id="tabs-1">
-
-            
-            <div>
-              
             <form class="form-horizontal">
-
                 <fieldset class=" bloque1">
-                    
                     <div class="titulo-cuadrado">
                         <p class="titulo">Actualiza tus datos aquí</p>
                         <p class="mini-letra-color">(Los datos marcos con * son obligatorios)</p>
@@ -29,13 +25,13 @@
                     <div class='row'>
                         <div class='col-md-6'>
                             <div class='form-group col-md-12'>
-                                <label for="name">Nombres:<span class="mini-letra-for">*</span></label>
+                                <label for="name">Nombres:<span class="mini-letra-color">*</span></label>
                                 <input class="form-control" id="name" name="name" placeholder="Nombre"/>
                             </div>
                         </div>
                         <div class='col-md-6'>
                             <div class='form-group col-md-12'>
-                                <label for="lastname">Apellidos:<span class="mini-letra-for">*</span></label>
+                                <label for="lastname">Apellidos:<span class="mini-letra-color">*</span></label>
                                 <input class="form-control" id="lastname" name="lastname"  placeholder="Apellidos"/>
                             </div>
                         </div>
@@ -44,7 +40,7 @@
                     <div class='row'>
                         <div class='col-sm-12'>
                             <div class='form-group col-md-4'>
-                                <label>Email: <span class="mini-letra-for">*</span></label>
+                                <label>Email: <span class="mini-letra-color">*</span></label>
                                 <input type="email" class="form-control" placeholder="Email">
                             </div>
                         </div>
@@ -93,7 +89,6 @@
                             
                         </div>
                     </div>
-                    
                 </fieldset>
                 
                 
@@ -101,18 +96,81 @@
                     <input type="submit" class="btn-lg btn-primary" value="Guardar Cambios"/>
                 </div>
                 
-
-                  
-
-                
               </form>
-            </div>
-
-
         </div>
         
-        <div id="tabs-2">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.</div>
-        <div id="tabs-3">Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.</div>
+        <!-- TAB SPORT-->
+        <div id="tabs-2">
+            <form class="form-horizontal">
+                <fieldset class=" bloque1">
+                    <div class='form-group '>
+                        <div class="col-md-12">
+                            <label for="name">Practico Deportes de Aventura desde el Año:<span class="mini-letra-color">(opcional)</span></label>
+                        </div>
+                        <div class="col-md-4">
+                            <select class="form-control" name="sportAt" id="sportAt">
+                                <option>2015</option>
+                                <option>2014</option>
+                                <option>2013</option>
+                                <option>2012</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+                
+                
+                <fieldset class=" bloque1">
+                    <div class='form-group '>
+                        <div class="col-md-12">
+                            <label for="name">Mis Deportes de Aventura favoritos son:<span class="mini-letra-color">(opcional)</span></label>
+                        </div>
+                        
+                        <div class="col-md-12">
+                            <span class="chk-item-deport"><input type="checkbox" id="inlineCheckbox1" value="1">4x4</span>
+                            <span class="chk-item-deport"><input type="checkbox" id="inlineCheckbox1" value="2">Andinismo</span>
+                            <span class="chk-item-deport"><input type="checkbox" id="inlineCheckbox1" value="3">Buceo</span>
+                            <span class="chk-item-deport"><input type="checkbox" id="inlineCheckbox1" value="4">Canotaje</span>
+                        </div>
+                    </div>
+                </fieldset>
+                
+                <div class="text-center">
+                    <input type="submit" class="btn-lg btn-primary" value="Guardar Cambios"/>
+                </div>
+            </form>
+        </div>
+        
+        <!-- TAB IMAGE-->
+        <div id="tabs-3">
+            
+            <div class="titulo-cuadrado">
+                <p class="titulo">Tu Foto</p>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="image.jpg" class="img-responsive"/>
+                </div>
+                <div class="col-md-8">
+                    <fieldset class="bloque1" >
+                    
+                        <h2>Sube una foto aquí:</h2>
+                        
+                        
+                        <div class="text-center">
+                            <input type="button" class="btn-lg" value="Subir Foto" />
+                        </div>
+                    </fieldset>
+                    
+                </div>
+                
+            </div>
+            
+            
+            
+            
+        </div>
+        
     </div>
 
 </div>
